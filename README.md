4651d5994c0ed4262f2547388d9e4ec8211d81f9
BSChampion <breevaneerden@outlook.com>
2022-04-30 12:29:09 +1000

    reset button

--- README.md
+++ README.md
@@ -6,7 +6,10 @@
 ## Plan
 9 box's with abilty to expand by using var gridNum
 Change player turn managed by var counter % 2
-class's to change on click
+class's to change on click - too long
+answer array - shorter
+stop double clicking on boxes
+stop game after win
 
 ## Cool tech you used (or code you're proud of)
 
@@ -14,7 +17,23 @@ class's to change on click
 ## Lessons you learnt
 
 ##Expanding ideas
-reset button (would you like to play again
+Completed - reset button (would you like to play again)
 Names of Players
 Computer Player
 Score card - best to 5
+
+
+
+learnt that although the testing and made this look like it worked... logged both winning
+for (var i = 0; i < players.length; i++) {
+    if ((ansArr[0] && ansArr[1] && ansArr[2] === players[i] ) || 
+        (ansArr[3] && ansArr[4] && ansArr[5] === players[i] ) || 
+        (ansArr[6] && ansArr[7] && ansArr[8] === players[i] ) || 
+        (ansArr[0] && ansArr[3] && ansArr[6] === players[i] ) || 
+        (ansArr[1] && ansArr[4] && ansArr[7] === players[i] ) || 
+        (ansArr[2] && ansArr[5] && ansArr[8] === players[i] ) || 
+        (ansArr[0] && ansArr[4] && ansArr[8] === players[i] ) || 
+        (ansArr[2] && ansArr[4] && ansArr[6] === players[i] )) {
+        console.log( ansArr + players[i] + " wins")
+    }
+}
--- tictactoe.js
+++ tictactoe.js
@@ -1,39 +1,57 @@
 var gridNum = 3
 //userinput for gridNum to expand or drop down list
 var counter = 1
-var messageBoard
-
+var messageBoard = document.querySelector('.messageBoard')
+var resetBtn = document.querySelector('.reset')
 var ansArr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 var players = ['X', 'O']
 
 function markBox(event) {
     var boxClickedOn = event.target
-    var allDivBox = document.querySelectorAll('div')
-    
-    counter = counter + 1
+    console.log(counter)
 
-    if (boxClickedOn.tagName === 'DIV') {
+    if ((boxClickedOn.tagName === 'DIV') && (ansArr[boxClickedOn.id] === 0)) {
+        counter = counter + 1
         if (counter % 2) {
             event.target.textContent = 'X'
-            ansArr.splice(event.target.id, 1, 'X')
+            messageBoard.textContent = "Player O's turn"
+            ansArr.splice(boxClickedOn.id, 1, 'X')
         } else {
             boxClickedOn.textContent = 'O'
-            ansArr.splice(event.target.id, 1, 'O')}
-
-        
+            messageBoard.textContent = "Player X's turn"
+            ansArr.splice(boxClickedOn.id, 1, 'O')}
+       // winning conditions 
     for (var i = 0; i < players.length; i++) {
-        if ((ansArr[0] === players[i]) && (ansArr[1] === players[i]) && (ansArr[2] === players[i]) || (ansArr[3] === players[i]) && (ansArr[4] === players[i]) && (ansArr[5] === players[i]) || (ansArr[6] === players[i]) && (ansArr[7] === players[i]) && (ansArr[8] === players[i]) || (ansArr[0] === players[i]) && (ansArr[3] === players[i]) && (ansArr[6] === players[i]) || (ansArr[1] === players[i]) && (ansArr[4] === players[i]) && (ansArr[7] === players[i]) || (ansArr[2] === players[i]) && (ansArr[5] === players[i]) && (ansArr[8] === players[i]) || (ansArr[0] === players[i]) && (ansArr[4] === players[i]) && (ansArr[8] === players[i]) || (ansArr[2] === players[i]) && (ansArr[4] === players[i]) && (ansArr[6] === players[i])) {
-            console.log( players[i] + " wins")
+        if (counter === (gridNum*gridNum)+1) {
+            messageBoard.textContent = "TIE"
+            resetBtn.textContent = "Click here to play again"
+
+        } else if ((ansArr[0] === players[i]) && (ansArr[1] === players[i]) && (ansArr[2] === players[i]) || (ansArr[3] === players[i]) && (ansArr[4] === players[i]) && (ansArr[5] === players[i]) || (ansArr[6] === players[i]) && (ansArr[7] === players[i]) && (ansArr[8] === players[i]) || (ansArr[0] === players[i]) && (ansArr[3] === players[i]) && (ansArr[6] === players[i]) || (ansArr[1] === players[i]) && (ansArr[4] === players[i]) && (ansArr[7] === players[i]) || (ansArr[2] === players[i]) && (ansArr[5] === players[i]) && (ansArr[8] === players[i]) || (ansArr[0] === players[i]) && (ansArr[4] === players[i]) && (ansArr[8] === players[i]) || (ansArr[2] === players[i]) && (ansArr[4] === players[i]) && (ansArr[6] === players[i])) {
+            messageBoard.textContent = "Player " + players[i] + " wins"
+            resetBtn.textContent = "Click here to play again"
+            ansArr = [1, 1, 1, 1, 1, 1, 1, 1, 1]
         }
     }
-    
-if (counter === 10) {
-    console.log("tie")
-}
+    }
 
-}
 }
 
 document
     .querySelector('.gameBoard')
     .addEventListener('click', markBox)
+
+function reset(event) {
+    var allDivBox = document.querySelectorAll('div')
+
+    ansArr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    counter = 1
+    resetBtn.textContent = "Reset"
+
+    for (i = 0; i < allDivBox.length; i++) {
+        allDivBox[i].textContent = ''
+    }
+} 
+
+resetBtn
+    .addEventListener('click', reset)
+
