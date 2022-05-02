49a0e7ff0326468569956b12a76e8c7e4ee8b140
BSChampion <breevaneerden@outlook.com>
2022-05-02 13:22:25 +1000

    stop clicking

--- tictactoe.js
+++ tictactoe.js
@@ -6,10 +6,18 @@ var resetBtn = document.querySelector('.reset')
 var ansArr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 var players = ['X', 'O']
 var allDivBox = document.querySelectorAll('div')
+var counter2 = 1
+// var elem2 = 'https://bit.ly/38CawXZ'
+// var elem = document.createElement("img");
+//   elem.setAttribute("src", "https://media3.giphy.com/media/xvZOuJeLij0W9IwCdH/giphy.gif?cid=790b7611f7d6f1d8b24b92cbf17ad6945c454e19c22b887d&rid=giphy.gif&ct=g");
+//   elem.setAttribute("height", "100");
+//   elem.setAttribute("width", "100");
+//   elem.setAttribute("alt", "1");
+// https://media4.giphy.com/media/3o7WTqRKlVRj0wsYQo/giphy.gif?cid=ecf05e475o9goo8i63ltmgqkm0de9joeyo1nost2d8mlhu6c&rid=giphy.gif&ct=g
 
 function markBox(event) {
     var boxClickedOn = event.target
-    console.log(counter)
+    console.log(counter)    
 
     if ((boxClickedOn.tagName === 'DIV') && (ansArr[boxClickedOn.id] === 0)) {
         counter = counter + 1
@@ -17,18 +25,47 @@ function markBox(event) {
             event.target.textContent = 'X'
             messageBoard.textContent = "Player O's turn"
             ansArr.splice(boxClickedOn.id, 1, 'X')
+            if (document.querySelector('.player1Name').textContent !== "Player 1") {
+                boxClickedOn.textContent = ''
+                var elem = document.createElement("img");
+                elem.setAttribute("src", "https://media4.giphy.com/media/3o7WTqRKlVRj0wsYQo/giphy.gif?cid=ecf05e475o9goo8i63ltmgqkm0de9joeyo1nost2d8mlhu6c&rid=giphy.gif&ct=g")
+                elem.setAttribute("height", "100")
+                elem.setAttribute("width", "100")
+                elem.setAttribute("alt", "1")
+                boxClickedOn.appendChild(elem)
+            }
         } else {
             boxClickedOn.textContent = 'O'
             messageBoard.textContent = "Player X's turn"
-            ansArr.splice(boxClickedOn.id, 1, 'O')}
+            ansArr.splice(boxClickedOn.id, 1, 'O')
+            if (document.querySelector('.player2Name').textContent !== "Player 2") {
+                boxClickedOn.textContent = ''
+                var elem = document.createElement("img");
+                elem.setAttribute("src", "https://media3.giphy.com/media/xvZOuJeLij0W9IwCdH/giphy.gif?cid=790b7611f7d6f1d8b24b92cbf17ad6945c454e19c22b887d&rid=giphy.gif&ct=g")
+                elem.setAttribute("height", "100")
+                elem.setAttribute("width", "100")
+                elem.setAttribute("alt", "1")
+                boxClickedOn.appendChild(elem)
+            }
+
+            }
+        } else if (counter === (gridNum*gridNum)+1) {
+            counter = counter + 1
+            messageBoard.textContent = "TIE"
+            ansArr = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+            resetBtn.textContent = "Click here to play again"
+        } else if (counter === (gridNum*gridNum)+2) {
+            counter = counter + 1
+            messageBoard.textContent = "Please click the Play Again button to play another game"
+        } else if (counter >= (gridNum*gridNum)+3) {
+            messageBoard.textContent = "Stop clicking the game is over!!!"
+        }
+
        // winning conditions 
     for (var i = 0; i < players.length; i++) {
-        if (counter === (gridNum*gridNum)+1) {
-            messageBoard.textContent = "TIE"
-            resetBtn.textContent = "Click here to play again"
-
-        } else if ((ansArr[0] === players[i]) && (ansArr[1] === players[i]) && (ansArr[2] === players[i]) || (ansArr[3] === players[i]) && (ansArr[4] === players[i]) && (ansArr[5] === players[i]) || (ansArr[6] === players[i]) && (ansArr[7] === players[i]) && (ansArr[8] === players[i]) || (ansArr[0] === players[i]) && (ansArr[3] === players[i]) && (ansArr[6] === players[i]) || (ansArr[1] === players[i]) && (ansArr[4] === players[i]) && (ansArr[7] === players[i]) || (ansArr[2] === players[i]) && (ansArr[5] === players[i]) && (ansArr[8] === players[i]) || (ansArr[0] === players[i]) && (ansArr[4] === players[i]) && (ansArr[8] === players[i]) || (ansArr[2] === players[i]) && (ansArr[4] === players[i]) && (ansArr[6] === players[i])) {
+        if ((ansArr[0] === players[i]) && (ansArr[1] === players[i]) && (ansArr[2] === players[i]) || (ansArr[3] === players[i]) && (ansArr[4] === players[i]) && (ansArr[5] === players[i]) || (ansArr[6] === players[i]) && (ansArr[7] === players[i]) && (ansArr[8] === players[i]) || (ansArr[0] === players[i]) && (ansArr[3] === players[i]) && (ansArr[6] === players[i]) || (ansArr[1] === players[i]) && (ansArr[4] === players[i]) && (ansArr[7] === players[i]) || (ansArr[2] === players[i]) && (ansArr[5] === players[i]) && (ansArr[8] === players[i]) || (ansArr[0] === players[i]) && (ansArr[4] === players[i]) && (ansArr[8] === players[i]) || (ansArr[2] === players[i]) && (ansArr[4] === players[i]) && (ansArr[6] === players[i])) {
             messageBoard.textContent = "Player " + players[i] + " wins"
+            counter = (gridNum*gridNum)+2
             resetBtn.textContent = "Click here to play again"
             ansArr = [1, 1, 1, 1, 1, 1, 1, 1, 1]
             if (players[i] === 'X') {
@@ -37,14 +74,14 @@ function markBox(event) {
                 document.querySelector('p.playerOScore').textContent = Number(document.querySelector('p.playerOScore').textContent)+1
             }
         }
+        }
     }
-    }
-
-}
+    
 
 document
     .querySelector('.gameBoard')
     .addEventListener('click', markBox)
+
 
 function reset(event) {
     ansArr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -59,8 +96,6 @@ function reset(event) {
 resetBtn
     .addEventListener('click', reset)
 
-
-
 function customise(event) {
     document.querySelector('.player1Name').textContent = prompt("Please enter the name for player 1")
     document.querySelector('.player2Name').textContent = prompt("Please enter the name for player 2")
@@ -70,34 +105,40 @@ customiseBtn =
     document.querySelector('.customise')
     .addEventListener('click', customise)
 
-function computerPlayerMove(event) {
-    counter = counter + 1
-    computerMove = Math.floor(Math.random() * (9 - 0))
-    console.log(counter)
+
+
+
+
+
+// function theme(event) {
+        
+//     }
+    
+// ThemeBtn =
+//         document.querySelector('.theme')
+//         .addEventListener('click', theme)
+
+// function computerPlayerMove(event) {
+    
+//     computerMove = Math.floor(Math.random() * (9 - 0))
+//     console.log(counter)
 
     
-    if (ansArr[computerMove] === 0) {        
-        if (counter % 2) {
-            allDivBox[computerMove].textContent = 'X'
-            messageBoard.textContent = "Player O's turn"
-            ansArr.splice(allDivBox[computerMove], 1, 'X')
-        } else {
-            allDivBox[computerMove].textContent = 'O'
-            messageBoard.textContent = "Player X's turn"
-            ansArr.splice(allDivBox[computerMove].id, 1, 'O')}
-    }
+//     if (ansArr[computerMove] === 0) {
+//         counter = counter + 1        
+//         if (counter % 2) {
+//             allDivBox[computerMove].textContent = 'X'
+//             messageBoard.textContent = "Player O's turn"
+//             ansArr.splice(allDivBox[computerMove], 1, 'X')
+//         } else {
+//             allDivBox[computerMove].textContent = 'O'
+//             messageBoard.textContent = "Player X's turn"
+//             ansArr.splice(allDivBox[computerMove].id, 1, 'O')}
+//     }
    
     
-}
-
-computerPlayerMoveBtn = 
-    document.querySelector('.computerPlayerMove')
-    .addEventListener('click', computerPlayerMove)
-
-// var winningConditions = [012, 345, 678, 036, 147, 258, 048, 246]
-
-// for (var i = 0; i < winningConditions.length; i++) {
-//     if ((ansArr[winningConditions[i][0]] === players[i]) && (ansArr[winningConditions[i][0]] === players[i]) && (ansArr[2] === players[i])
 // }
 
-// } else if ((ansArr[0] === players[i]) && (ansArr[1] === players[i]) && (ansArr[2] === players[i]) || (ansArr[3] === players[i]) && (ansArr[4] === players[i]) && (ansArr[5] === players[i]) || (ansArr[6] === players[i]) && (ansArr[7] === players[i]) && (ansArr[8] === players[i]) || (ansArr[0] === players[i]) && (ansArr[3] === players[i]) && (ansArr[6] === players[i]) || (ansArr[1] === players[i]) && (ansArr[4] === players[i]) && (ansArr[7] === players[i]) || (ansArr[2] === players[i]) && (ansArr[5] === players[i]) && (ansArr[8] === players[i]) || (ansArr[0] === players[i]) && (ansArr[4] === players[i]) && (ansArr[8] === players[i]) || (ansArr[2] === players[i]) && (ansArr[4] === players[i]) && (ansArr[6] === players[i])) {
+// computerPlayerMoveBtn = 
+//     document.querySelector('.computerPlayerMove')
+//     .addEventListener('click', computerPlayerMove)
